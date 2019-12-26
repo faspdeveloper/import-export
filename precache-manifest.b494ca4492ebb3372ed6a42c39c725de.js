99fbc8d906ca96a5b72b923c4af2342e75d24a23
Dolly Chhabriya <dolly.c@altius.cc>
2019-12-26 11:12:26 +0530

    Updates

---
+++ precache-manifest.b494ca4492ebb3372ed6a42c39c725de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d4936ddff962e22b76ae517756d346",
+    "url": "/import-export/index.html"
+  },
+  {
+    "revision": "bda01c30a681aec04ce5",
+    "url": "/import-export/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "6953a5d82af113ec9b7c",
+    "url": "/import-export/static/css/main.814884c2.chunk.css"
+  },
+  {
+    "revision": "bda01c30a681aec04ce5",
+    "url": "/import-export/static/js/2.7cb8f6b8.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/import-export/static/js/2.7cb8f6b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6953a5d82af113ec9b7c",
+    "url": "/import-export/static/js/main.90ce02c4.chunk.js"
+  },
+  {
+    "revision": "b57762a33619e4caf5e5",
+    "url": "/import-export/static/js/runtime-main.4d841339.js"
+  }
+]);
